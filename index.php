ad825ad7177a0435813b950552d748c35f2a4ec5
luciobo <luciobo@icloud.com>
2022-12-21 14:06:06 +0100

    commit inizale

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,12 +10,13 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
 
 <body>
 
-<h1 class=""></h1>
+    <div class=""></div>
 
 </body>
 
