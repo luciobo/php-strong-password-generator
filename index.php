4071591b227053e54b6dbaae752dd5a2dda46d55
luciobo <luciobo@icloud.com>
2022-12-21 16:06:26 +0100

    aggiunto form

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>vue-email-list</title>
+    <title>php-strong-password-generator</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
@@ -16,7 +19,20 @@
 
 <body>
 
-    <div class=""></div>
+    <div>
+        <div class="row justify-content-center py-5">
+            <div class="col-6">
+                <h1 class="text-center pb-5 text-primary">Genera La Tua Password</h1>
+                <form method="POST">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Inserisci un numero per generare la password</label>
+                        <input type="number" class="form-control" name="numero">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Genera Password</button>
+                </form>
+            </div>
+        </div>
+    </div>
 
 </body>
 
